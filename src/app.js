643592bc1a838808849c63fbaf67b1c5b9670d84
melo_gustavo <gustavomelo95@gmail.com>
2020-03-24 11:50:42 -0300

    Exception error handling with sentry and youch

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+// Essa vc importa dessa maneira porque o express por padrao nao vai passar para o sentry os erros que der em operacoes async
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,11 +13,16 @@ class App {
   constructor() {
     this.server = express();
 
+    // Isso eh para verificacao de erros... Tipo um gerenciador de erros
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     // Middlewares global
     this.server.use(express.json());
     // libernado para que vc consiga clicar nos links das fotos e abrir elas
@@ -24,6 +34,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // Quando um middleware recebe quatro parametros, quer dizer que ele eh uma middleware de tratamento de excecoes
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://0c035bd03fee448383ee51bdefc06a2e@sentry.io/5173378',
+};
